e4a475d3e9d8c959114a42f12302d03ea28d6b98
cabosh <callum.bosher@gmail.com>
2020-06-03 15:31:00 +0100

    Attempted commit

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,11 @@
 def main():
     #write your code below this line
+    number = int(input("Enter a number: "))
+
+    if (number % 2) == 0:
+        print(f"The number {number} is even.")
+    else:
+        print(f"The number {number} is odd.")
 
 if __name__ == '__main__':
     main()
